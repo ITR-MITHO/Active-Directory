17ff6d8c025d52fb894ba95594d1a44e74b93f30
Mikkel Thodsen <123701392+ITR-MITHO@users.noreply.github.com>
2023-01-31 13:27:25 +0000

    Update InactiveUsers.ps1

--- InactiveUsers.ps1
+++ InactiveUsers.ps1
@@ -7,7 +7,7 @@ To change the search scope, change the number of days from "90" to "XX" where XX
 
 Import-Module ActiveDirectory
 $Date = (Get-Date).AddDays(-90)
-Get-ADuser  -filter * -Properties DisplayName, SamAccountName, LastLogonDate, PasswordLastSet, Manager | Where {$_.LastLogonDate -LT $Date} | Select DisplayName, SamAccountName, LastLogonDate, PasswordLastSet, Manager, Enabled, @{n='OU';e={$_.distinguishedname -replace '^.+?,(CN|OU.+)','$1'}} |
+Get-ADuser  -filter * -Properties DisplayName, SamAccountName, LastLogonDate, PasswordLastSet, Manager, WhenCreated | Where {$_.LastLogonDate -LT $Date} | Select DisplayName, SamAccountName, WhenCreated LastLogonDate, PasswordLastSet, Manager, Enabled, @{n='OU';e={$_.distinguishedname -replace '^.+?,(CN|OU.+)','$1'}} |
 Export-csv $Home\Desktop\InactiveUsers.csv -Notypeinformation -Encoding Unicode
 
 Write-Host "Find the exported data here: $home\Desktop\InactiveUsers.csv" -ForegroundColor Green
