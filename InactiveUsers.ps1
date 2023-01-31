8b30cb00c40288d32fe21b9ef3e4737056ae9a54
Mikkel Thodsen <123701392+ITR-MITHO@users.noreply.github.com>
2023-01-31 16:13:19 +0000

    Update InactiveUsers.ps1

--- InactiveUsers.ps1
+++ InactiveUsers.ps1
@@ -1,7 +1,7 @@
 <#
 
 Finds users that haven't logged in within the last 90 days. 
-To change the search scope, change the number of days from "90" to "XX" where XX is how many days you want to look back.
+To change the search scope, change the number of days from "90" to "XX" where XX is how many days it should filter from
 
 #>
 
