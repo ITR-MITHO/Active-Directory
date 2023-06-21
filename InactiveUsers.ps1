b6804eb1f28ad62487310a8bdef01b22f57b1bcf
Mikkel Thodsen <123701392+ITR-MITHO@users.noreply.github.com>
2023-06-21 13:36:52 +0000

    Update InactiveUsers.ps1

--- InactiveUsers.ps1
+++ InactiveUsers.ps1
@@ -1,7 +1,15 @@
+# Checking permissions
+$PMError = Test-Path $Home\desktop\PermissionIssue.txt
+if ($PMError)
+{
+Remove-Item "$Home\desktop\PermissionIssue.txt" -Force
+}
+timeout 3
 $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
 If (-not $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
 {
-write-host "Script is not running as Administrator" -ForegroundColor Yellow
+echo "Start PowerShell as an Administrator" > $Home\desktop\PermissionIssue.txt
+Start $home\desktop\PermissionIssue.txt
 Break
 }
 
