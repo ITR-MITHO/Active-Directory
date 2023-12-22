9ef7339f69290cc4628d966f29e2b0bf581e2331
Mikkel Thodsen <123701392+ITR-MITHO@users.noreply.github.com>
2023-12-22 09:04:19 +0000

    Update FindGPOSetting.ps1

--- FindGPOSetting.ps1
+++ FindGPOSetting.ps1
@@ -3,7 +3,6 @@
 The script will ask you to enter the word to search for. Whatever you type in, the script will wildcard search for. If you enter "Outlook" it will find anything that have Outlook inside of it, from Office templates, to registry keys.
 
 #>
-
 $Phrase = Read-Host "Enter what setting you want to search for, the script will wildcard search"
 
 $DC = (Get-ADDomainController | Select Name -First 1).Name
