c27859a034ab66e2e5057baeb0c2fda28549b594
Mikkel Thodsen <123701392+ITR-MITHO@users.noreply.github.com>
2024-02-07 15:33:18 +0000

    Update AD-Security.ps1

--- AD-Security.ps1
+++ AD-Security.ps1
@@ -1,4 +1,25 @@
-# Stop and disable Print Spooler on all domain controllers - 10 points
+<#
+
+  THIS SCRIPT IS NOT READY FOR PRODUCTION, IT IS STILL BEING TESTED AND DEVELOPED. RUN AT YOUR OWN RISK
+
+  Functionality:
+  Stops and DISABLES the Print Spooler service on all domain controllers
+  Enables AD Recycle Bin (If not already enabled)
+  Removes all members from Schema Admins and Enterprise Admins
+  Prevents all domain admins from being delegated in the domain
+  Enables PowerShell logging on all domain controllers
+  Change primary group for all users to "Domain Users"
+  Disables NTLMV1 and only allows NTLMV2 by registry
+  Protects all Orginizational Units from accidential deletion
+  Creates a csv-file containing all AD-users that have a password that never expires
+  Creates a file listing the Default Domain Password Policy
+  Creates a file listing the audit policies
+  Creates a csv-file containing all administrator accounts
+
+#>
+
+
+# Stop and disable Print Spooler on all domain controllers
 $DomainName = (Get-ADDomain).DNSRoot
 $DC = Get-ADDomainController -filter * | Select Hostname
 Foreach ($D in $DC)
@@ -10,7 +31,7 @@ Set-Service Spooler -StartupType Disabled
     }
 Write-Host "INFORMATION: Print spoolers stopped and disabled" -ForegroundColor Yellow
 
-# Enable AD Recycle Bin if not already present - 15 points
+# Enable AD Recycle Bin if not already present
 If (-Not (Get-ADOptionalFeature -Filter {Name -like "Recycle*"}).EnabledScopes)
 {
 $BinDestination = Get-ADOptionalFeature "Recycle Bin Feature" | Select DistinguishedName
@@ -18,7 +39,7 @@ Enable-ADOptionalFeature $BinDestination.DistinguishedName -Scope ForestOrConfig
 Write-Host "INFORMATION: AD Recycle bin enabled" -ForegroundColor Yellow
 }
 
-# Empty the Schema Admins and Enterprise Admins - 10 points
+# Empty the Schema Admins and Enterprise Admins
 $Schema = Get-ADGroupMember -Identity "Schema Admins" | Get-ADUser -Properties SamAccountName
 Foreach ($S in $Schema)
 {
@@ -31,7 +52,7 @@ Remove-ADGroupMember -Identity "Enterprise Admins" -Members $E.SamaccountName -C
 }
 Write-Host "INFORMATION: Removed all members in Schema Admins and Enterprise Admins" -ForegroundColor Yellow
 
-# Prevent administrator accounts from being delegated - 20 points
+# Prevent administrator accounts from being delegated
 Get-ADGroupMember "Domain Admins" | Get-ADuser -Properties AccountNotDelegated | Where-Object {-not $_.AccountNotDelegated -and $_.ObjectClass -EQ "User"} | Set-ADUser -AccountNotDelegated $True
 Write-Host "INFORMATION: All members of Domain Admins set to not allow delegation" -ForegroundColor Yellow
 
@@ -49,7 +70,15 @@ Set-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\Po
     }
 Write-Host "INFORMATION: Enabled Powershell audit logging" -ForegroundColor Yellow
 
-# Disable NTLMV1 and only allow NTLMV2 - 15 points - DISABLED SINCE IT CAN IMPACT PRODUCTION
+# Set Primary Group to 'Domain Users' for anyone but the 'Guest' account
+$PrimaryGroup = Get-ADUser -Filter * -Properties SamAccountName, PrimaryGroup | Where-Object { $_.PrimaryGroup -ne (Get-ADGroup -Identity "Domain Users").DistinguishedName -and $_.SamAccountName -NE "Guest"}
+Foreach ($Primary in $PrimaryGroup)
+{
+Set-ADUser $Primary.SamAccountName -Replace @{PrimaryGroupID='513'}
+}
+Write-Host "INFORMATION: Changed primary group of all users to 'Domain Users'" -ForegroundColor Yellow
+
+# Disable NTLMV1 and only allow NTLMV2  - DISABLED SINCE IT CAN IMPACT PRODUCTION
 <#Foreach ($D in $DC)
 {
 Invoke-Command $D.HostName {
