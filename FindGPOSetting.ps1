3c294df70cc858ef2163ee48c68b097552b797ac
MITHO <123701392+ITR-MITHO@users.noreply.github.com>
2025-02-20 12:40:50 +0100

    Update FindGPOSetting.ps1

--- FindGPOSetting.ps1
+++ FindGPOSetting.ps1
@@ -1,9 +1,4 @@
-<#
-
-The script will ask you to enter the word to search for. Whatever you type in, the script will wildcard search for. If you enter "Outlook" it will find anything that have Outlook inside of it, from Office templates, to registry keys.
-
-#>
-$Phrase = Read-Host "Enter what setting you want to search for, the script will wildcard search"
+# Replace "Outlook" in line 10 with whatever you need to search for
 
 $DC = (Get-ADDomainController | Select Name -First 1).Name
 Invoke-Command -ComputerName $DC {
@@ -12,7 +7,7 @@ $AllGPO = Get-GPO -All -Domain $env:SERDNSDOMAIN
 
 ForEach ($GPO in $AllGPO) { 
     $Report = Get-GPOReport -Guid $GPO.Id -ReportType XML 
-    if ($Report -match $Phrase) { 
+    if ($Report -match 'Outlook') { 
         Write-Host "$($GPO.DisplayName)" -ForeGroundColor "Green"
         $MatchedGPOList += "$($GPO.DisplayName)";
 } 
